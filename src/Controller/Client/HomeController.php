07f723306c6f29fdf2ec6d245b314236bc7acac0
ilef17 <ilef.zayed138@gamil.com>
2026-02-21 20:45:20 +0100

    modifications sur les routes

--- src/Controller/Client/HomeController.php
+++ src/Controller/Client/HomeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('', name: 'app_home')]
+    #[Route('/client/dashboard', name: 'app_home')]
     public function index(): Response
     {
         // Calculate greeting based on time of day
